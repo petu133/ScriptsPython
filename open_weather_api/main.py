94314007349edd562c02b400106a10a571ec42ad
petu133 <77856779+SmurfRegulus@users.noreply.github.com>
2022-05-18 21:01:05 -0300

    get and show information function done

--- open_weather_api/main.py
+++ open_weather_api/main.py
@@ -7,28 +7,38 @@ import os
 
 api_key = os.environ.get('APIKEY')
 
-def getWeather():
+def getWeather(display): #This function needs to inherit the tkinder portrait to show the information
     city = entryField.get()
     url_call = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
     json_data = requests.get(url_call).json()
-    
+    condition = json_data['weather'][0]['main']
+    temperature = int(json_data['main']['temp'] - 273)
+    pressure = json_data['main']['pressure']
+    wind = json_data['wind']['speed']
+    min_t = int(json_data['main']['temp_min'] - 273)
+    max_t = int(json_data['main']['temp_max'] - 273)
+    country = json_data['sys']['country']
+    latitude = json_data['coord']['lat']
+    longitude = json_data['coord']['lon']
+    info_principal = f"Current Condition: {condition} \n Current Temperature: {temperature}° Celsius"
+    info_secondary = f"Latitude: {latitude} \nLongitude: {longitude} \nCountry: {country} \nPressure: {pressure}\"hPa\" \nWind: {wind} \"meters per second\" \nMinimum Temperature: {min_t}°C \nMaximum Temperature: {max_t}°C \n"
+    first_label.config(text = info_principal)
+    second_label.config(text = info_secondary)
 
+#creating widget
 display = tk.Tk()
 display.title("Open Weather")
-display.size("600x500")
+display.geometry("1000x600")
 
-type_font = ("Arial", 20, "bold")
-type_font2 = ("Calibri", 30, "roman")
-# label_font_two = ("Calibri", 25, "italic")
-
-entryField = tk.Entry(display, font = type_font)
-entryField.pack(pady=25)
-entryField.focus()
-
-first_label = tk.Label(display, font = type_font)
+font_principal = ("arial", 40, "bold")
+font_secundary = ("calibri", 20, "italic")
+entryField = tk.Entry(display, justify='center', font = font_principal)
+entryField.pack(fill= None, side='bottom')
+entryField.focus() #determines that this field is going to receive the imput from the user 
+entryField.bind('<Return>', getWeather)
+first_label = tk.Label(display, font = font_principal)
 first_label.pack()
-second_label = tk.Label(display, font = type_font2)
+second_label = tk.Label(display, font = font_secundary)
 second_label.pack()
-
-display.mainloop()
+display.mainloop() #set endpoint for the widget
 
